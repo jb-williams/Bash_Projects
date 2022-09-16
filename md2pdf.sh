97bb5bb85d477fcfd84965028e035257a114f2cf
xfafnir <xfafnir@ol-rogue>
2022-09-15 19:55:12 -0500

    Updated alot of programs to be mor interactive

---
+++ md2pdf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+pandoc "$1" \
+    -f gfm \
+    --include-in-header chapter_break.tex \
+    -V linkcolor:blue \
+    -V geometry:a4paper \
+    -V geometry:margin=2cm \
+    -V mainfont="DejaVu Serif" \
+    -V monofont="DejaVu Sans Mono" \
+    --pdf-engine=xelatex \
+    --toc
+    -o "$2"
